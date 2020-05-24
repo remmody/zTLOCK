2149d511892facf11131142b3b9b02e5850107c0
RemmoDY <remmody@yandex.ru>
2020-05-25 00:22:36 +0500

    Initial commit

---
+++ src/js/background/background.js
@@ -0,0 +1,38 @@
+let _hash = "fcd170118fe0c5cb454972c4215a0700";
+
+let blacklist = [
+	"main-frame",
+	"minimized",
+	"movable",
+	"window",
+	"header",
+	"tab",
+	"minimize-btn",
+	"content",
+	"cnt_minimize_window",
+	"ui-draggable",
+	"ui-draggable-handle",
+	"dompath=",
+	"stack"
+]
+
+chrome.webRequest.onBeforeRequest.addListener(
+	function (details) {
+		let result = false;
+		if(details.url.indexOf("main.swf") != -1){
+			if(details.url.indexOf(_hash) == -1){
+				
+			}
+		}
+		blacklist.forEach(item => {
+			if (details.url.indexOf(item) != -1){
+				result = true;
+			}
+		});
+		return {
+			cancel: result
+		};
+	}, {
+		urls: ["https://*.bigpoint.net/*", "https://darkorbit-22.bpsecure.com/spacemap/main.swf*"]
+	}, ["blocking"]
+);
